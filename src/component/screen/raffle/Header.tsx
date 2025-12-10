3709f3327c3afcd6af2aaf5ce08c824b696aa53f
Dean Garcia <myEmail@gmail.com>
2025-12-10 02:44:30 -0600

    Adjusted top bar to be one line

--- src/component/screen/raffle/Header.tsx
+++ src/component/screen/raffle/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "../../../styles.css";
 import {
   Button,
@@ -60,6 +60,7 @@ const Header = ({
   const drawnEntries = useSelector(selectDrawnEntries);
   const dispatch = useDispatch();
   const isRaffleRunning = useSelector(selectIsRaffleActive);
+
   const handleChange = (event: any) => {
     if (isRaffleRunning) return;
     dispatch(updateCurrentRaffle(event.target.value));
@@ -86,9 +87,57 @@ const Header = ({
 
   const lastDrawnEntry = drawnEntries.slice(-1)[0]?.split("-")[0] ?? "";
   const winnerName = winners[activeRaffle]?.split("-")[0];
-  const winnerText = `${winnerName} has won the ${activeRaffle} raffle!`;
+  const winnerText = `${winnerName} is the winner!`;
 
   const headerText = winnerName ? winnerText : lastDrawnEntry;
+
+  const getTitleText = () => {
+    let text;
+    let fontSize = "3rem";
+    console.log("activeraffle", activeRaffle);
+    switch (activeRaffle) {
+      case "Home Depot":
+        text = "Home Depot $500 Gift Card";
+        break;
+      case "Soothe":
+        text = "Soothe $500 Gift Card";
+        break;
+      case "RRR":
+        text = "Roemerman Ranch Retreat";
+        break;
+      case "KitchenAid":
+        text = "KitchenAid Stand Mixer & Williams Sonoma $500 Gift Card";
+        fontSize = "3rem";
+        break;
+      case "Kamado Joe":
+        text = "Kamado Joe Classic Joe II";
+        break;
+      case "Grand Prix":
+        text = "Indy Car Grand Prix Arlington - 2 Reserved Weekend Passes";
+        fontSize = "3rem";
+        break;
+      case "PS5":
+        text = "PS5 Bundle";
+        break;
+      case "Nintendo":
+        text = "Nintendo Bundle";
+        break;
+      case "TV":
+        text = `85" TV with Soundbar`;
+        break;
+      case "Apple":
+        text = `13" iPad Air & AirPods Pro 3`;
+        break;
+      case "Travel":
+        text = `$2500 Travel Voucher`;
+        break;
+      default:
+        text = "No Raffle Selected";
+    }
+    return [text, fontSize];
+  };
+
+  const [title, fontSize] = getTitleText();
 
   return (
     <div className="header">
@@ -102,12 +151,24 @@ const Header = ({
         </Button>
       </div>
       <div className="header-text">
-        <div style={{ fontSize: "5rem", color: "white" }}>{activeRaffle}</div>
         <div
           style={{
-            fontSize: "2rem",
+            fontSize: fontSize,
             color: "white",
-            textWrapMode: "nowrap",
+            padding: "0px 50px 0px 50px",
+            // border: "1px solid white",
+          }}
+        >
+          {title}
+        </div>
+        <div
+          style={{
+            fontSize: "3rem",
+            color: "white",
+            alignItems: "center",
+            justifyContent: "center",
+            verticalAlign: "center",
+            display: "flex",
           }}
         >
           {headerText}
